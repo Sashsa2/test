25bc281bf9366b33d98924704befea4e1fd3c482
Sashsa2 <97801236+Sashsa2@users.noreply.github.com>
2022-01-23 10:31:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// var firstnum = prompt("ВВЕДИТЕ 1 ЧИСЛО")
+// // var secondnum = prompt("ВВЕДИТЕ 2 ЧИСЛО")
+// // alert(+firstnum + Number(secondnum))
+// // console.log(typeof(firstnum))
+// // var id = document.querySelector("#js")
+// // var el = document.querySelector("#btn")
+// // btn.onclick = function(){
+// // 	id.innerHTML = "ХЕЙ Я МОРЖ"
+// // 	id.style.color = "#FF4543"
+// // }
+//  var wr = document.querySelector("#wr")
+//  console.log(wr)
+ 
+//  var btn = document.querySelector("#btn")
+//  console.log(btn)
+
+// btn.onclick = function(){
+// 	wr.style.background = "#808080"
+// }
+
+
+
+var age = +prompt("Сколько Вам лет?")
+age < 18 ? alert("Тебе суда низя") : 
+age == 20 ? alert ("Тебе 20") : alert("Привте незнакомец")
